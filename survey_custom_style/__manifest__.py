e3a5e67bac8b41b46bd891244d6cb758c4858a77
AmalAldbiby <amaldu2@gmail.com>
2025-11-11 15:48:12 +0300

    Add survey_custom_style model

---
+++ survey_custom_style/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Survey Custom Theme',
+    'version': '17.0.1.0.0',
+    'category': 'Survey',
+    'summary': """survey custom style""",
+    'description': """Students behavior and Discipline""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "http://www.educationalerp.com",
+    'depends': ['base',
+    'survey'],
+    'assets': {
+    'web.assets_frontend': [
+    'survey_custom_style/static/src/css/custom_survey.css',
+    ],
+    },
+    'application': True,
+}
